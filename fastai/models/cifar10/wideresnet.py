d716c309ea62581c085aa2fed7f5c896909e235d
Andrew Shaw <andrew.t.shaw@gmail.com>
2018-05-03 12:45:11 -0400

    Adding a bunch of tests for speed and training accuracy

--- fastai/models/cifar10/wideresnet.py
+++ fastai/models/cifar10/wideresnet.py
@@ -21,20 +21,21 @@ def bn_relu_conv(ni, nf, ks, stride, init_zero=False):
 def noop(x): return x
 
 class BasicBlock(nn.Module):
-    def __init__(self, ni, nf, stride, drop_p=0.0):
+    def __init__(self, ni, nf, stride, drop_p=0.0, scale=0.4):
         super().__init__()
         self.bn = nn.BatchNorm2d(ni)
         self.conv1 = conv_2d(ni, nf, 3, stride)
         self.conv2 = bn_relu_conv(nf, nf, 3, 1)
         self.drop = nn.Dropout(drop_p, inplace=True) if drop_p else None
         self.shortcut = conv_2d(ni, nf, 1, stride) if ni != nf else noop
+        self.scale = scale
 
     def forward(self, x):
         x2 = F.relu(self.bn(x), inplace=True)
         r = self.shortcut(x2)
         x = self.conv1(x2)
         if self.drop: x = self.drop(x)
-        x = self.conv2(x) * 0.2
+        x = self.conv2(x) * self.scale
         return x.add_(r)
 
 
@@ -57,8 +58,25 @@ class WideResNet(nn.Module):
 
     def forward(self, x): return self.features(x)
 
+    
+class WideResNetConcat(nn.Module):
+    def __init__(self, num_groups, N, num_classes, k=1, drop_p=0.0, start_nf=16):
+        super().__init__()
+        n_channels = [start_nf]
+        for i in range(num_groups): n_channels.append(start_nf*(2**i)*k)
+
+        layers = [conv_2d(3, n_channels[0], 3, 1)]  # conv1
+        for i in range(num_groups):
+            layers += _make_group(N, n_channels[i], n_channels[i+1], BasicBlock, (1 if i==0 else 2), drop_p)
+
+        layers += [nn.BatchNorm2d(n_channels[3]), nn.ReLU(inplace=True), AdaptiveConcatPool2d(1),
+                   Flatten(), nn.Linear(n_channels[3]*2, num_classes)]
+        self.features = nn.Sequential(*layers)
+
+    def forward(self, x): return self.features(x)
 
 def wrn_22(): return WideResNet(num_groups=3, N=3, num_classes=10, k=6, drop_p=0.)
+def wrn_22_cat(): return WideResNetConcat(num_groups=3, N=3, num_classes=10, k=6, drop_p=0.)
 def wrn_22_k8(): return WideResNet(num_groups=3, N=3, num_classes=10, k=8, drop_p=0.)
 def wrn_22_k10(): return WideResNet(num_groups=3, N=3, num_classes=10, k=10, drop_p=0.)
 def wrn_22_k8_p2(): return WideResNet(num_groups=3, N=3, num_classes=10, k=8, drop_p=0.2)
